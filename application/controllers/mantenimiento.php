9016f51b79d86f9476a657ba39cc4e49bf7df9d8
Tavorz <tavo_grz@live.com.mx>
2014-10-14 10:26:55 -0500

    Añadido Modelo

---
+++ application/controllers/mantenimiento.php
@@ -0,0 +1,14 @@
+<?php
+ class Mantenimiento_model extends CI_Model {
+ 
+ function Mantenimiento_model() {
+ parent::__construct(); //llamada al constructor de Model.
+ }
+ 
+function getData() {
+ $usuarios = $this->db->get('profesor'); //obtenemos la tabla 'contacto'. db->get('nombre_tabla') equivale a SELECT * FROM nombre_tabla.
+ 
+ return $usuarios->result(); //devolvemos el resultado de lanzar la query.
+ }
+}
+?>
